7a64a2262b8df4cadff9332a35fc0608b2becca3
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-01-22 06:20:30 -0600

    removed unused forms

--- forms.py
+++ forms.py
@@ -1,60 +1,7 @@
-from flask_wtf import FlaskForm, Form
-from wtforms import StringField, PasswordField, SelectField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
-states = [
-    ('AK', 'Alaska'),
-    ('AL', 'Alabama'),
-    ('AR', 'Arkansas'),
-    ('AZ', 'Arizona'),
-    ('CA', 'California'),
-    ('CO', 'Colorado'),
-    ('CT', 'Connecticut'),
-    ('DC', 'District of Columbia'),
-    ('DE', 'Delaware'),
-    ('FL', 'Florida'),
-    ('GA', 'Georgia'),
-    ('HI', 'Hawaii'),
-    ('IA', 'Iowa'),
-    ('ID', 'Idaho'),
-    ('IL', 'Illinois'),
-    ('IN', 'Indiana'),
-    ('KS', 'Kansas'),
-    ('KY', 'Kentucky'),
-    ('LA', 'Louisiana'),
-    ('MA', 'Massachusetts'),
-    ('MD', 'Maryland'),
-    ('ME', 'Maine'),
-    ('MI', 'Michigan'),
-    ('MN', 'Minnesota'),
-    ('MO', 'Missouri'),
-    ('MS', 'Mississippi'),
-    ('MT', 'Montana'),
-    ('NC', 'North Carolina'),
-    ('ND', 'North Dakota'),
-    ('NE', 'Nebraska'),
-    ('NH', 'New Hampshire'),
-    ('NJ', 'New Jersey'),
-    ('NM', 'New Mexico'),
-    ('NV', 'Nevada'),
-    ('NY', 'New York'),
-    ('OH', 'Ohio'),
-    ('OK', 'Oklahoma'),
-    ('OR', 'Oregon'),
-    ('PA', 'Pennsylvania'),
-    ('RI', 'Rhode Island'),
-    ('SC', 'South Carolina'),
-    ('SD', 'South Dakota'),
-    ('TN', 'Tennessee'),
-    ('TX', 'Texas'),
-    ('UT', 'Utah'),
-    ('VA', 'Virginia'),
-    ('VT', 'Vermont'),
-    ('WA', 'Washington'),
-    ('WI', 'Wisconsin'),
-    ('WV', 'West Virginia'),
-    ('WY', 'Wyoming')
-]
 
 class UserSignUpForm(FlaskForm):
     """Form for adding a new user."""
@@ -86,23 +33,5 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=8)])
-
-class ShippingAddressForm(FlaskForm):
-    """Form for editting shipping or billing addresses."""
-
-    address_line_1 = StringField('Address Line 1', validators=[DataRequired()])
-    address_line_2 = StringField('(Optional) Address Line 2')
-    state = SelectField('State', choices=states, validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    zip_code = StringField('XXXXX', validators=[Length(min=5)])
-
-class BillingAddressForm(FlaskForm):
-    """Form for editting shipping or billing addresses."""
-
-    address_line_1 = StringField('Address Line 1', validators=[DataRequired()])
-    address_line_2 = StringField('(Optional) Address Line 2')
-    state = SelectField('State', choices=states, validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    zip_code = StringField('XXXXX', validators=[Length(min=5)])
 
 
